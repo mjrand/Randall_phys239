52de58d4402bd69f3fa1e4c57b1f68f2bd49cd73
Michael Randall <59715680+mjrand@users.noreply.github.com>
2020-09-29 00:06:11 -0700

    Update Hello_World.py

--- HW1/Hello_World.py
+++ HW1/Hello_World.py
@@ -7,8 +7,12 @@ Created on Mon Sep 28 23:48:21 2020
 from tkinter import Tk
 from tkinter import messagebox
 
+#Create dialogue box object and raise it to the topof the display
 root = Tk()
 root.lift()
+
+#Create and display message box 
 messagebox.showinfo("Hello", "Hello World!")
 
+#Destroy the object after the message box is closed
 root.destroy()
